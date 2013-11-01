ae7f4bdd40c6db87050e97f8a01fee8c84d57647
Steve Kondik <steve@cyngn.com>
2013-11-01 20:28:10 +0000

    power: Add new power hints
     * New hint to support CPU boosting with a duration.
    
    Change-Id: I79b5a33814f2dbeaa033c950264d430876505175
    
    libhardware: Add audio power hint
    
    Change-Id: I5b03cc060356da23b6941c5937ef217c70dbf6dd
    
    libhardware: Add power hint to set profile
    
     * A PowerHAL can implement support for this hint to receive
       power profile changes from the framework.
    
    Change-Id: Ie1e9e3b827c731cf5a817a0491677e3451fe8678
    
    libhardware: Add POWER_HINT_LAUNCH_BOOST for PowerHAL
    
    Change-Id: I9f2387ad273a376da77f9360f1e620f4cd9f861c
    
    hardware: Add new getFeature() function to Power HAL
    
     * The current use case for this is to query the power HAL for
       the number of supported profiles.
    
    Change-Id: I0b4137dda8d98d167d9c6446072bdbe1245ae2f5

--- include/hardware/power.h
+++ include/hardware/power.h
@@ -63,11 +63,18 @@ typedef enum {
     POWER_HINT_VIDEO_DECODE = 0x00000004,
     POWER_HINT_LOW_POWER = 0x00000005,
     POWER_HINT_SUSTAINED_PERFORMANCE = 0x00000006,
-    POWER_HINT_VR_MODE = 0x00000007
+    POWER_HINT_VR_MODE = 0x00000007,
+
+    POWER_HINT_CPU_BOOST    = 0x00000010,
+    POWER_HINT_LAUNCH_BOOST = 0x00000011,
+    POWER_HINT_AUDIO        = 0x00000020,
+    POWER_HINT_SET_PROFILE  = 0x00000030
+
 } power_hint_t;
 
 typedef enum {
-    POWER_FEATURE_DOUBLE_TAP_TO_WAKE = 0x00000001
+    POWER_FEATURE_DOUBLE_TAP_TO_WAKE = 0x00000001,
+    POWER_FEATURE_SUPPORTED_PROFILES = 0x00001000
 } feature_t;
 
 /*
@@ -246,6 +253,11 @@ typedef struct power_module {
      *     provide minimum guarantee for performance for the amount of time the
      *     device can sustain it. The data parameter is non-zero when the mode
      *     is activated and zero when deactivated.
+     * POWER_HINT_CPU_BOOST
+     *
+     *     An operation is happening where it would be ideal for the CPU to
+     *     be boosted for a specific duration. The data parameter is an
+     *     integer value of the boost duration in microseconds.
      *
      * A particular platform may choose to ignore any hint.
      *
@@ -324,6 +336,12 @@ typedef struct power_module {
      * availability: version 0.5
      */
     int (*get_voter_list)(struct power_module *module, size_t *voter);
+
+    /*
+     * (*getFeature) is called to get the current value of a particular
+     * feature or capability from the hardware or PowerHAL
+     */
+    int (*getFeature)(struct power_module *module, feature_t feature);
 
 } power_module_t;
 
